945d607375e0a8b57c1bce4cf82baeb5556af10b
Ishan Tandukar <077bct019.ishan@sagarmatha.edu.np>
2025-07-15 07:31:18 +0545

    first task completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Task2 from "./components/Task2.jsx";
+
+function App() {
+  return (
+    <>
+      <Task2 />
+    </>
+  );
+}
+
+export default App;
